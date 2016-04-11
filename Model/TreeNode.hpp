0650d628ea261757fea9755dc8cf9628109fe0ac
ghop1073 <gagehopkins1@gmail.com>
2016-04-11 11:59:28 -0600

    added my tree node class

---
+++ Model/TreeNode.hpp
@@ -0,0 +1,14 @@
+//
+//  TreeNode.hpp
+//  NodeProjectXCode
+//
+//  Created by Hopkins, Michael on 4/11/16.
+//  Copyright © 2016 Gage Hopkins. All rights reserved.
+//
+
+#ifndef TreeNode_hpp
+#define TreeNode_hpp
+
+#include "Node.h"
+
+#endif /* TreeNode_hpp */
