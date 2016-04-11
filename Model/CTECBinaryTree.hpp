9459b9d36a5962a360351998ec23f18cfe7268a7
ghop1073 <gagehopkins1@gmail.com>
2016-04-11 13:18:46 -0600

    finished binaryTree.hpp class file

--- Model/CTECBinaryTree.hpp
+++ Model/CTECBinaryTree.hpp
@@ -12,12 +12,25 @@
 #include "TreeNode.hpp"
 namespace CTECData
 {
+    template <class Type>
     class CTECBinaryTree
     {
     private:
-        
+        int size;
+        TreeNode<Type> * root;
+        int height;
+        bool balanced;
+        bool contains(Type value, CTECBinaryTree<Type> * currentTree);
     public:
-        
+        CTECBinaryTree();
+        ~CTECBinaryTree();
+        bool insert(const Type& value);
+        Type remove(const Type& value);
+        bool contains(Type value);
+        int getSize();
+        int getHeight();
+        bool isBalanced();
+        TreeNode<Type> * getRoot();
     };
 }
 
