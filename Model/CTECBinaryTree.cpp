ae9cb40b13b4c597afc6b85481eac4598792dfba
ghop1073 <gagehopkins1@gmail.com>
2016-04-11 13:10:46 -0600

    created cTECBinaryTree class

---
+++ Model/CTECBinaryTree.cpp
@@ -0,0 +1,9 @@
+//
+//  CTECBinaryTree.cpp
+//  NodeProjectXCode
+//
+//  Created by Hopkins, Michael on 4/11/16.
+//  Copyright © 2016 Gage Hopkins. All rights reserved.
+//
+
+#include "CTECBinaryTree.hpp"
---
+++ Model/CTECBinaryTree.hpp
@@ -0,0 +1,24 @@
+//
+//  CTECBinaryTree.hpp
+//  NodeProjectXCode
+//
+//  Created by Hopkins, Michael on 4/11/16.
+//  Copyright © 2016 Gage Hopkins. All rights reserved.
+//
+
+#ifndef CTECBinaryTree_hpp
+#define CTECBinaryTree_hpp
+
+#include "TreeNode.hpp"
+namespace CTECData
+{
+    class CTECBinaryTree
+    {
+    private:
+        
+    public:
+        
+    };
+}
+
+#endif /* CTECBinaryTree_hpp */
